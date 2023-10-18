349e099cc50d57677c698fd1b6fa77c400849c03
Jeremy Patak <jeremypatak16@gmail.com>
2023-10-17 23:04:46 -0500

    move data to googlesheets and authenticate access

--- log/app.R
+++ log/app.R
@@ -1,7 +1,8 @@
 library(pacman)
 p_load(shiny, magrittr, dplyr, stringr, gt, gtExtras)
 
-transaction_log = read.csv("transaction_log.csv")
+transaction_log = read_sheet("https://docs.google.com/spreadsheets/d/1Ti24DVdNRNHGZ4IKGJ342dCDSvycJtdfC8OFh0jlLEI/edit?usp=sharing")
+
 
 players = transaction_log %>% 
   group_by(trans_ID, to_team) %>%
@@ -16,25 +17,29 @@ picks = transaction_log %>%
   group_by(trans_ID, to_team) %>%
   arrange(asset) %>% 
   filter(substr(asset, 1, 2) == '20' | substr(asset, 1, 19) == "Cash Considerations") %>%
-  mutate(picks = paste(asset, collapse = ", ")) %>%
-  select(trans_ID, to_team, picks) %>%
+  mutate(picks = paste(asset, collapse = ", "),
+         notes = paste(note, collapse = ", ")) %>%
+  select(trans_ID, to_team, picks, notes) %>%
   unique()
 
 incoming_by_team = full_join(players, picks, by = c('trans_ID', 'to_team')) %>%
   mutate(players = ifelse(is.na(players), "", players)) %>%
   mutate(picks = ifelse(is.na(picks), "", picks)) %>%
-  select(trans_ID, to_team, players, picks) %>%
+  mutate(notes = ifelse(is.na(notes), "", notes)) %>%
+  select(trans_ID, to_team, players, picks, notes) %>%
   ungroup() %>%
   arrange(trans_ID, to_team) %>%
   mutate(first = c(1, diff(trans_ID)) == 1) %>%
   left_join(hoopR::espn_nba_teams(), by = c("to_team" = "display_name")) %>%
-  select(trans_ID, to_team, logo_dark, players, picks, first)
+  select(trans_ID, to_team, logo_dark, players, picks, first, notes)
 
 rm(players, picks)
 
 gt_transaction_log = incoming_by_team %>%
   mutate(players = str_replace_all(players, ", ", "<br>"),
-         picks = str_replace_all(picks, ", ", "<br>")) %>% 
+         picks = str_replace_all(picks, ", ", "<br>"), 
+         notes = str_replace_all(notes, ", ", "<br>"),
+         notes = str_replace_all(notes, "NA", " ")) %>% 
   gt() %>%
   gt_img_rows(columns = logo_dark, height = 75) %>%
   gt_theme_pff() %>%
@@ -46,9 +51,9 @@ gt_transaction_log = incoming_by_team %>%
   #vertical align in players and picks cells
   tab_style(
     style = "vertical-align:top",
-    locations = cells_body(columns = c("players", "picks"))) %>%
+    locations = cells_body(columns = c("players", "picks", "notes"))) %>%
   cols_hide(columns = c(trans_ID, to_team, first)) %>%
-  fmt_markdown(columns = c("players", "picks")) %>%
+  fmt_markdown(columns = c("players", "picks", "notes")) %>%
   cols_width(players ~ px(200),
              logo_dark ~ px(200),
              picks ~ px(200)) %>%
@@ -61,7 +66,7 @@ ui = navbarPage("2023 Mock Trade Deadline", fluid = TRUE,
                          fluidRow(column(9, h1(span("2023 Mock Trade Deadline", style = 'color:rgba(169,20,20,128);')), 
                                          h1(span("Transaction Log", style = 'font-size: 60px; font-weight: bold;'))),
                                   column(3, img(src="ASU-NTDC-Logo.png", height = 180, width = 240))),
-                         fluidRow(12, gt_output("TransactionLog"))))
+                         fluidRow(column(12, gt_output("TransactionLog")))))
 
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
