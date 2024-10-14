22efdbdf1bf85897b78842315f4d07abe548936a
Jeremy Patak <jeremypatak16@gmail.com>
2024-10-14 11:58:49 -0500

    created UI outline and started making inputs

--- Apps_2024/data_entry/app.R
+++ Apps_2024/data_entry/app.R
@@ -12,12 +12,11 @@ gs4_auth(path = ".secrets/asu-mtd-c191882d31e3.json")
 ss <- gs4_get("https://docs.google.com/spreadsheets/d/13TtpNrsgFBv9UR4s-mw15uDd17UruiV9OZM6fWs03kE/edit?usp=sharing")
 
 hoopR_espn_nba_teams <- read.csv("espn_nba_teams.csv")
-nba_players = read.csv("nba_players.csv")
 
 nba_teams <- hoopR_espn_nba_teams |> 
   pull(display_name)
 
-# Define UI for application that draws a histogram
+
 ui = navbarPage("2024 NBA Trade Deadline Competition", fluid = TRUE,
                 tabPanel(
                   "Trade Entry",
@@ -38,8 +37,9 @@ ui = navbarPage("2024 NBA Trade Deadline Competition", fluid = TRUE,
                       img(src="ASU-NTDC-Logo.png", height = 180, width = 240)
                     )
                   ),
-                  tags$hr(style="border-color:rgba(169,20,20,128);"),
+                  tags$hr(style="border-color:rgba(169,20,20,128); margin-bottom:0px"),
                   fluidRow(
+                    style = "background-color: #dddddd; padding-top: 20px; padding-bottom: 20px; display: flex; align-items: center;",
                     column(
                       6, 
                       selectizeInput(
@@ -64,22 +64,196 @@ ui = navbarPage("2024 NBA Trade Deadline Competition", fluid = TRUE,
                       )
                     )
                   ),
-                  h2(" ")
-                ),
-              )
+                  # Conditional Panel for 1 Team
+                  fluidRow(
+                    conditionalPanel(
+                      condition = "input.teamsInvolved.length == 1",
+                      column(
+                        12,
+                        div(
+                          style = "text-align: center;",
+                          htmlOutput("team1_logo"),
+                          h4("Trading Away:")
+                        )
+                      )
+                    )
+                  ),
+                  # Conditional Panel for 2 Teams
+                  fluidRow(
+                    conditionalPanel(
+                      condition = "input.teamsInvolved.length == 2",
+                      column(
+                        6,
+                        div(
+                          style = "text-align: center; border-right: 2px solid black; height: 100%;",
+                          htmlOutput("team1_logo2")
+                        ),
+                        column(
+                          8,
+                          h4("Players Traded:"),
+                          uiOutput("t1p1"),
+                          uiOutput("t1p2")
+                        ),
+                        column(
+                          4,
+                          h4("To:"),
+                          uiOutput("t1p1_to")
+                        )
+                      ),
+                      column(
+                        6,
+                        div(
+                          style = "text-align: center;",
+                          htmlOutput("team2_logo"),
+                          h4("Trading Away:")
+                        )
+                      )
+                    )
+                  ),
+                  # Conditional Panel for 3 Teams
+                  fluidRow(
+                    conditionalPanel(
+                      condition = "input.teamsInvolved.length == 3",
+                      column(
+                        4,
+                        div(
+                          style = "text-align: center; border-right: 2px solid black; height: 100%;",
+                          htmlOutput("team1_logo3"),
+                          h4("Trading Away:")
+                        )
+                      ),
+                      column(
+                        4,
+                        div(
+                          style = "text-align: center; border-right: 2px solid black; height: 100%;",
+                          htmlOutput("team2_logo3"),
+                          h4("Trading Away:")
+                        )
+                      ),
+                      column(
+                        4,
+                        div(
+                          style = "text-align: center;",
+                          htmlOutput("team3_logo"),
+                          h4("Trading Away:")
+                        )
+                      )
+                    )
+                  )
+                )
+)
 
-# Define server logic required to draw a histogram
+
 server <- function(input, output, session) {
   
   transaction_log = reactiveVal(
-      value = read_sheet("https://docs.google.com/spreadsheets/d/13TtpNrsgFBv9UR4s-mw15uDd17UruiV9OZM6fWs03kE/edit?usp=sharing")
+      value = read_sheet(
+        "https://docs.google.com/spreadsheets/d/13TtpNrsgFBv9UR4s-mw15uDd17UruiV9OZM6fWs03kE/edit?usp=sharing",
+        sheet = "All_TL"
+        )
     )
 
-  #clear page on button click
+  # Clear page on button click
   observeEvent(input$clearpg, {
     session$reload()
   })
-
+  
+  # Logo outputs for teams
+  logo1_url <- reactive(
+      hoopR_espn_nba_teams |>
+        filter(
+          display_name == input$teamsInvolved[[1]]
+        ) |>
+        pull(logo)
+    )
+  
+  output$team1_logo = output$team1_logo2 = output$team1_logo3 = renderText({
+      paste0(
+        '<img src ="', 
+        logo1_url(), 
+        '" style="width:75px;">'
+      )
+    })
+  
+  logo2_url <- reactive(
+    hoopR_espn_nba_teams |>
+      filter(
+        display_name == input$teamsInvolved[[2]]
+      ) |>
+      pull(logo)
+  )
+  
+  output$team2_logo = output$team2_logo3 = renderText({
+    paste0(
+      '<img src ="', 
+      logo2_url(), 
+      '" style="width:75px;">'
+    )
+  })
+  
+  logo3_url <- reactive(
+    hoopR_espn_nba_teams |>
+      filter(
+        display_name == input$teamsInvolved[[3]]
+      ) |>
+      pull(logo)
+  )
+  
+  output$team3_logo = renderText({
+    paste0(
+      '<img src ="', 
+      logo3_url(), 
+      '" style="width:75px;">'
+    )
+  })
+  
+  ########## UI Outputs for Inputs
+  
+  ### Traded Player Inputs
+  output$t1p1 <- renderUI({
+    if(FALSE){}
+    else{
+      selectizeInput(
+        "t1p1", 
+        "", 
+        choices = read_sheet(
+            ss,
+            sheet = hoopR_espn_nba_teams |>
+              filter(
+                display_name == input$teamsInvolved[[1]]
+              ) |>
+              pull(abbreviation)
+          ) |>
+          mutate(
+            Player = ifelse(is.na(Player), "", Player)
+          ) |>
+          pull(Player), 
+        options = list(create = TRUE)
+      )
+    }
+  })
+  output$t1p2 <- renderUI({
+    if(input$t1p1 == " " | input$t1p1 == ""){}
+    else{
+      selectizeInput(
+        "t1p2", 
+        "", 
+        choices = read_sheet(
+          ss,
+          sheet = hoopR_espn_nba_teams |>
+            filter(
+              display_name == input$teamsInvolved[[1]]
+            ) |>
+            pull(abbreviation)
+        ) |>
+          mutate(
+            Player = ifelse(is.na(Player), "", Player)
+          ) |>
+          pull(Player), 
+        options = list(create = TRUE)
+      )
+    }
+  })
 
 }
 
